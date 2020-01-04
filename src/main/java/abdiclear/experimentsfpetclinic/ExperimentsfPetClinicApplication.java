e2b8d4b5ea48ddee6c1d325d0281ae680c13c7d0
abdiclear <moham137@uwindsor.ca>
2020-01-04 14:30:44 -0500

    Initial CommitAdded

---
+++ src/main/java/abdiclear/experimentsfpetclinic/ExperimentsfPetClinicApplication.java
@@ -0,0 +1,13 @@
+package abdiclear.experimentsfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExperimentsfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentsfPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/abdiclear/experimentsfpetclinic/ExperimentsfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package abdiclear.experimentsfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExperimentsfPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
